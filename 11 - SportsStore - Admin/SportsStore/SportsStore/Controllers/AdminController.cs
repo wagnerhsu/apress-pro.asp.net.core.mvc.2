0e904f070e422478f9f26c085f19668702342993
Wagner Xu <wagnerhsu@hotmail.com>
2019-10-07 15:40:27 +0800

    Upgrade to netcoreapp3.0

--- 11 - SportsStore - Admin/SportsStore/SportsStore/Controllers/AdminController.cs
+++ 11 - SportsStore - Admin/SportsStore/SportsStore/Controllers/AdminController.cs
@@ -2,12 +2,14 @@
 using SportsStore.Models;
 using System.Linq;
 
-namespace SportsStore.Controllers {
-
-    public class AdminController : Controller {
+namespace SportsStore.Controllers
+{
+    public class AdminController : Controller
+    {
         private IProductRepository repository;
 
-        public AdminController(IProductRepository repo) {
+        public AdminController(IProductRepository repo)
+        {
             repository = repo;
         }
 
@@ -18,12 +20,16 @@ namespace SportsStore.Controllers {
                 .FirstOrDefault(p => p.ProductID == productId));
 
         [HttpPost]
-        public IActionResult Edit(Product product) {
-            if (ModelState.IsValid) {
+        public IActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
                 repository.SaveProduct(product);
                 TempData["message"] = $"{product.Name} has been saved";
                 return RedirectToAction("Index");
-            } else {
+            }
+            else
+            {
                 // there is something wrong with the data values
                 return View(product);
             }
@@ -32,9 +38,11 @@ namespace SportsStore.Controllers {
         public ViewResult Create() => View("Edit", new Product());
 
         [HttpPost]
-        public IActionResult Delete(int productId) {
+        public IActionResult Delete(int productId)
+        {
             Product deletedProduct = repository.DeleteProduct(productId);
-            if (deletedProduct != null) {
+            if (deletedProduct != null)
+            {
                 TempData["message"] = $"{deletedProduct.Name} was deleted";
             }
             return RedirectToAction("Index");
--- 14 - Configuring Applications/ConfiguringApps/ConfiguringApps/Program.cs
+++ 14 - Configuring Applications/ConfiguringApps/ConfiguringApps/Program.cs
@@ -1,45 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
+﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using System.Reflection;
+using System.IO;
 
-namespace ConfiguringApps {
-    public class Program {
-
-        public static void Main(string[] args) {
+namespace ConfiguringApps
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
             BuildWebHost(args).Run();
         }
 
-        public static IWebHost BuildWebHost(string[] args) {
-
+        public static IWebHost BuildWebHost(string[] args)
+        {
             return new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
-                .ConfigureAppConfiguration((hostingContext, config) => {
+                .ConfigureAppConfiguration((hostingContext, config) =>
+                {
                     var env = hostingContext.HostingEnvironment;
                     config.AddJsonFile("appsettings.json",
                         optional: true, reloadOnChange: true)
                     .AddJsonFile($"appsettings.{env.EnvironmentName}.json",
                             optional: true, reloadOnChange: true);
                     config.AddEnvironmentVariables();
-                    if (args != null) {
+                    if (args != null)
+                    {
                         config.AddCommandLine(args);
                     }
                 })
-                .ConfigureLogging((hostingContext, logging) => {
+                .ConfigureLogging((hostingContext, logging) =>
+                {
                     logging.AddConfiguration(
                         hostingContext.Configuration.GetSection("Logging"));
                     logging.AddConsole();
                     logging.AddDebug();
                 })
                 .UseIISIntegration()
-                .UseDefaultServiceProvider((context, options) => {
+                .UseDefaultServiceProvider((context, options) =>
+                {
                     options.ValidateScopes =
                         context.HostingEnvironment.IsDevelopment();
                 })
